213bb677ad8e5b00ef10669ce6b28d03d10be3d6
ehyoder <ehyoder@gmail.com>
2018-03-26 18:51:06 -0400

    weather hw draft

--- config.py
+++ config.py
@@ -1,2 +1,14 @@
 # beth's openweather api key
 api_key = "47c985bf6fe3331da440b6c66508ae72"
+
+api_key2 = "89ccd0f338237161c86ed521267f5afc"
+
+api_key3 = "d9d69c665021b5a3dbe1efa1218170ae"
+
+api_key4 = "6d62363e73145c32cc3558119c606914"
+
+api_key5 = "37c1dc8ca5c2a8d23cd1d07d4b899b95"
+
+api_key6 = "815d3611ff6c21e2f3dd6d6b5e01ba18"
+
+api_key7 = "600705590395534f06bdbc249160592b"
